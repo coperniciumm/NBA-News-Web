69e94e23e42ef501bde0080febf9b2d8d4d6ce0f
coper <joe2582038@gmail.com>
2018-07-29 23:26:54 +0800

    fix error

--- nba_news/urls.py
+++ nba_news/urls.py
@@ -21,7 +21,7 @@ from news.views import _list, index, post
 
 urlpatterns = [
     url(r'^index/$', views.index),
-    url(r'^post/(?P<pk>\d+)/$', views.post, name='post'),
+    url(r'^post/(?P<pk>\d+)', views.post, name='post'),
     url(r'^article/$', views._list, name='article'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
