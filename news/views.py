d92ff561cd77d33745bb1c7a648fa29ef2cf6931
coper <joe2582038@gmail.com>
2018-07-29 08:37:04 +0800

    fix views

--- news/views.py
+++ news/views.py
@@ -7,6 +7,7 @@ from rest_framework import viewsets
 #from rest_framework.response import Response
 #from rest_framework import status
 from django.http import HttpResponse, JsonResponse
+from rest_framework.decorators import api_view
 #from django.views.decorators.csrf import csrf_exempt
 #from rest_framework.renderers import JSONRenderer
 #from rest_framework.parsers import JSONParser
