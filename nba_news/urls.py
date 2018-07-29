7cb3346d3f4c64098e4b85c5037a88e1c8a65e4e
coper <joe2582038@gmail.com>
2018-07-29 08:43:21 +0800

    fix test page

--- nba_news/urls.py
+++ nba_news/urls.py
@@ -18,12 +18,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.urlpatterns import format_suffix_patterns
 from news import views
+from news.views import _list
 
 urlpatterns = [
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^index/$', views.index),
     url(r'^post/$', views.post),
-    url(r'^test/$', _list),
+    url(r'^test/$', views._list),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = format_suffix_patterns(urlpatterns)
